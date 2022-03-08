a97d41f598f7f5fdc3b0cd61dbd41adcf5c92cc0
Guilherme Andrade Rosa <gui.rsa@hotmail.com>
2022-03-08 15:21:29 -0300

    Requisitos 2,3 e 4

---
+++ src/components/input/input.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class input extends Component {
+  render() {
+    this.props = { type, datatestid }
+    return (
+      <div>input</div>
+    )
+  }
+}
